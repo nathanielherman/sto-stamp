028399c12a8ec53d61959506da1082034860ca46
Yihe Huang <yihehuang@g.harvard.edu>
2015-03-03 18:31:01 -0500

    tracking max transSet

--- stamp-0.9.10/lib/thread.c
+++ stamp-0.9.10/lib/thread.c
@@ -102,11 +102,12 @@ void reportPerf(){
     using thr = threadinfo_t;
     thr tc = Transaction::tinfo_combined();
     printf("STO System Shutdown:\n"
-           " read: %llu, write: %llu  searched: %llu\n"
-           " starts: %llu aborts: %llu commit time aborts: %llu\n"
-           " average set size: %llu\n",
+           " read: %llu, write: %llu, searched: %llu\n"
+           " starts: %llu, aborts: %llu, commit time aborts: %llu\n"
+           " average set size: %llu, max set size: %llu\n",
            LLU(tc.p[thr::p_total_r]), LLU(tc.p[thr::p_total_w]), LLU(tc.p[thr::p_total_searched]),
-           LLU(tc.p[thr::p_total_starts]), LLU(tc.p[thr::p_total_aborts]), LLU(tc.p[thr::p_commit_time_aborts]), LLU(tc.p[thr::p_total_n])/LLU(tc.p[thr::p_total_starts]));
+           LLU(tc.p[thr::p_total_starts]), LLU(tc.p[thr::p_total_aborts]), LLU(tc.p[thr::p_commit_time_aborts]),
+           LLU(tc.p[thr::p_total_n])/LLU(tc.p[thr::p_total_starts]), LLU(tc.p[thr::p_max_set]));
 #undef LLU
 }
 #if 0
