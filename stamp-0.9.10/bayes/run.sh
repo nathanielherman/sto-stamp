cc3d398fab3514e374ed88d5cc6ea6c5809cd657
jinala <Jeevu@JEEVANAs-MacBook-Pro.local>
2015-03-03 23:55:29 -0500

    Use CONFIG variable in the scripts

--- stamp-0.9.10/bayes/run.sh
+++ stamp-0.9.10/bayes/run.sh
@@ -2,11 +2,13 @@ RESDIR="./tmp"
 echo "Creating tmp directory " $RESDIR
 mkdir $RESDIR
 
+CONFIG="-v32 -r4096 -n10 -p40 -i2 -e8 -s1"
+
 make -f Makefile.seq clean > /dev/null 2>&1
 make -f Makefile.seq > /dev/null 2>&1
 
-echo "Seq run -v32 -r4096 -n10 -p40 -i2 -e8 -s1"
-./bayes -v32 -r4096 -n10 -p40 -i2 -e8 -s1 > $RESDIR/seq.txt
+echo "Seq run "$CONFIG
+./bayes $CONFIG > $RESDIR/seq.txt
 
 python getTime.py $RESDIR/seq.txt
 
@@ -15,8 +17,8 @@ make -f Makefile.STO > /dev/null 2>&1
 
 for t in 1 2 4 8 16 32
 do
-echo "STO run -v32 -r4096 -n10 -p40 -i2 -e8 -s1 -t"$t
-./bayes -v32 -r4096 -n10 -p40 -i2 -e8 -s1 -t $t > $RESDIR/sto$t.txt
+echo "STO run "$CONFIG" -t"$t
+./bayes $CONFIG -t$t > $RESDIR/sto$t.txt
 python getTime.py $RESDIR/sto$t.txt
 done
 
@@ -26,8 +28,8 @@ make -f Makefile.stm > /dev/null 2>&1
 
 for t in 1 2 4 8 16 32
 do
-echo "TL2 run -v32 -r4096 -n10 -p40 -i2 -e8 -s1 -t"$t
-./bayes -v32 -r4096 -n10 -p40 -i2 -e8 -s1 -t $t > $RESDIR/stm$t.txt
+echo "TL2 run "$CONFIG" -t"$t
+./bayes $CONFIG -t$t > $RESDIR/stm$t.txt
 python getTime.py $RESDIR/stm$t.txt
 done
 
