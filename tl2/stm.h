a76f0ace98af40972ce9b7045071f77ba073d4fd
Jeevana <jinala@mit.edu>
2015-03-10 16:23:42 -0400

    Tl2 bug fix  with float writes

--- tl2/stm.h
+++ tl2/stm.h
@@ -83,13 +83,13 @@ typedef volatile intptr_t               vintp;
 
 #define STM_WRITE(var, val)             TxStore(STM_SELF, \
                                                 (vintp*)(void*)&(var), \
-                                                (intptr_t)(val))
+                                                (intptr_t)(val), 0)
 #define STM_WRITE_F(var, val)           TxStore(STM_SELF, \
                                                 (vintp*)FP2IPP(&(var)), \
-                                                F2IP(val))
+                                                F2IP(val), 1)
 #define STM_WRITE_P(var, val)           TxStore(STM_SELF, \
                                                 (vintp*)(void*)&(var), \
-                                                VP2IP(val))
+                                                VP2IP(val), 0)
 
 #define STM_LOCAL_WRITE(var, val)       ({var = val; var;})
 #define STM_LOCAL_WRITE_F(var, val)     ({var = val; var;})
@@ -105,3 +105,4 @@ typedef volatile intptr_t               vintp;
  *
  * =============================================================================
  */
+
